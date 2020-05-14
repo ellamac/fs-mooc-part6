7d746c1df8d3a0b7f4660d0e2a07503693c47193
Ella Mäkelä <ella@makela.co>
2020-05-14 22:24:24 +0300

    tehtävät: 6.3-6.8

---
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { voteAnecdote } from '../reducers/anecdoteReducer';
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <div>
+      <div>{anecdote.content}</div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  );
+};
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch();
+  const anecdotes = useSelector((state) => state);
+
+  return (
+    <div>
+      {anecdotes
+        .sort((a, b) => a.votes - b.values)
+        .map((anecdote) => (
+          <Anecdote
+            key={anecdote.id}
+            anecdote={anecdote}
+            handleClick={() => dispatch(voteAnecdote(anecdote.id))}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default AnecdoteList;
